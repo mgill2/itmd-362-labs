ed940cc03ac0cd70b614646518905251d36f925f
mgill2 <mgill5@hawk.iit.edu>
2017-02-02 12:22:50 -0600

    added label for email and placeholder

--- Lab 1/js/site.js
+++ Lab 1/js/site.js
@@ -5,4 +5,17 @@ $.noConflict();
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  
+  $('#email').on('keyup focus blur', funciton()
+  {
+	  var currentValue = $(this).val();
+	  var validPattern = $(this).attr('pattern');
+	  if(currentValue.match(validPattern))
+	  {
+		  $('#submit').addClass('active');
+	  }
+	  else {
+		  $('#submit').removeClass('active');
+	  }
+  });
 });
